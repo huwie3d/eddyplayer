a81657a039ebd3efce69005e448dfd587633b496
natalie <22222885+espeon@users.noreply.github.com>
2024-11-26 01:33:30 -0600

    cracked the code

--- src/components/lyrics/Lyrics.tsx
+++ src/components/lyrics/Lyrics.tsx
@@ -112,14 +112,14 @@ export function Lyrics({
 
   // get the type of lyrics. is it jlf or parsed lyrics
   return (
-    <div className="flex-1 h-full relative text-white -mx-8 px-4 md:px-8 min-h-max">
+    <div className="flex-1 h-full relative overflow-x-hidden text-white -mx-8 px-4 md:px-8 min-h-max">
       <h2 className="z-10 absolute top-8 text-xl font-semibold hidden md:block">
         Lyrics
       </h2>
       <div className="blur-vignette" />
       <div
         ref={scrollContainerRef}
-        className={`hide-scrollbar space-y-4 overflow-y-auto -ml-1 md:ml-0 max-h-[88vh] ${isFullPage ? "md:max-h-[80vh]" : "md:max-h-[calc(33.5rem)] -mb-36 md:mb-0"} pr-4`}
+        className={`hide-scrollbar space-y-4 overflow-y-auto -ml-1 md:ml-0 max-h-[88vh] ${isFullPage ? "md:max-h-[calc(95vh-9rem)]" : "md:max-h-[calc(33.5rem)] -mb-36 md:mb-0"} pr-4`}
         style={{
           maskImage: `linear-gradient(to bottom, transparent 0rem, black 8rem, black 50%, #000${lessThanMd ? "1" : "5"} 85%, transparent 98%)`,
           maskComposite: "intersect",
